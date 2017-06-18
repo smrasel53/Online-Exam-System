a3ed98684387b4e7348d39df73223f723f6bd57e
Abu Sofian <smrasel.cse@gmail.com>
2017-06-18 15:21:19 +0600

    added documentation

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define("DB_HOST", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "");
-	define("DB_NAME", "exam_system");
+	define("DB_USER", "root"); // Write your your username here. By default: root 
+	define("DB_PASS", ""); // Write your your password here. By default: "" 
+	define("DB_NAME", "exam_system");  // Write your your Database name here.
 ?>	
